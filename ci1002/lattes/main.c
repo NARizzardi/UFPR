dfe0223fe5827037a0b1f2dbedcb185090fff7fc
NARizzardi <rizzanic2000@gmail.com>
2022-11-22 20:56:08 -0300

    [add] first schema for project

--- ci1002/lattes/main.c
+++ ci1002/lattes/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "lib_lattes.h"
+
+	/*
+	 * 1) apresentar a produção sumarizada do grupo por ordem de periódicos discriminando o estrato
+	 * 2) apresentar a produçãoi sumarizada do grupo por ordem de conferências
+	 * 3) apresentar a produção dos pesquisadores do grupo por ordem de autoria, em periódicos e conferências
+	 * 4) apresentar a produção sumarizada do grupo por ano, em periódicos e conferências
+	 * 5) listar aqueles periodicos e eventos classificados no nivel c
+	 * 6) listar aqueles periodicos e eventos não classificados
+	 * ./lattes -d <diretorio com os CVs> -c <arquivo com a lista Qualis Conf> -p <arquivo com a lista Qualis Periódicos>
+	*/
 
 int main(){
 	FILE *file_pointer;
