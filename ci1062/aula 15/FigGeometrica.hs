6f99e623c358784903eaec985611dedc2622cd33
NARizzardi <rizzanic2000@gmail.com>
2023-05-26 18:56:51 -0300

    [add] slides e exercicio aula 15
    Signed-off-by: NARizzardi <rizzanic2000@gmail.com>

---
+++ ci1062/aula 15/FigGeometrica.hs
@@ -0,0 +1,8 @@
+module FigGeometrica where
+    quadrado :: Int -> Int
+    retangulo :: Int -> Int -> Int
+    triangulo :: Float -> Float -> Float
+    
+    quadrado l = l * l
+    retangulo b a = b * a
+    triangulo b a = (b * a) / 2
---
+++ ci1062/aula 15/Operacoes.hs
@@ -0,0 +1,4 @@
+module Operacoes where
+mult x y = x * y
+soma x y = x + y
+
