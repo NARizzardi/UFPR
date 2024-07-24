bd1223b4b6d330ed745237ba9e6f33c619bcde63
nar20 <nar20@inf.ufpr.br>
2024-07-24 16:26:24 -0300

    [add] exercicio aula 08
    Signed-off-by: nar20 <nar20@inf.ufpr.br>

--- 1064/aula08/areaS.s
+++ 1064/aula08/areaS.s
@@ -4,6 +4,11 @@ calcula_area:
     pushq %rbp
     movq %rsp, %rbp
 
+    movq $3, %r9            # pi para r9
+    movq  +16(%rbp), %rax   # move o parametro para rax
+    imul %rax, %rax         # parametro ao quadrado
+    imul %r9, %rax          # pi vezes parametro
+
     popq %rbp
     ret
 
@@ -11,15 +16,14 @@ _start:
     pushq %rbp
     movq %rsp, %rbp
 
-    pushq $3
-    pushq -8(%rbp)
+    pushq $1                # int r = 3
+
+    pushq -8(%rbp)          # chama funcao calcula_area
     call calcula_area
-
-
     addq $8, %rsp
+
+    addq $8, %rsp           #retorno da funcao
     popq %rbp
-    
-
-
+    movq %rax, %rdi
     movq $60, %rax
     syscall
