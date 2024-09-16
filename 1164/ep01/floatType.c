1c2196a0fcd49219b27757f5c49f2f821a402225
NARizzardi <rizzanic2000@gmail.com>
2024-09-16 00:30:40 +0000

    icc ep01: change error constant

--- 1164/ep01/floatType.c
+++ 1164/ep01/floatType.c
@@ -5,9 +5,9 @@
 #include <math.h>
 #include "floatType.h"
 
-#define ERROR 0.000001
-
-
+/*
+ Function used to calculate the next term of the summatory
+*/
 double nextInteraction(int interaction_quantity){
     double ret = 1;
     double denominator, numerator;
@@ -37,19 +37,34 @@ double nextInteraction(int interaction_quantity){
     return ret * 2;
 }
 
+
+/*
+ Function used to check if the approximate absolute
+ error is lesser than the stipulated tolerance
+*/
 int lowerThanTolerance(Float_t tolerance, Float_t aprox_absolute_error){
-    if((aprox_absolute_error.f-tolerance.f) <= ERROR)
+    if((aprox_absolute_error.f-tolerance.f) <= DBL_EPSILON)
         return 1;
     return 0;
 }
 
+/*
+ Function used to conver the floating point number
+ to integer used in the hex print
+*/
 uint64_t doubleToHexCoverter(double d)
 {
   return (*(uint64_t*)&d);
 }
 
+/*
+ Function to calculate the error
+ it can be both approximate error
+ or the real error
+*/
 double calculateNewError(double value_n_interaction, double value_previous_interaction){
-    return value_n_interaction - value_previous_interaction;
+    double result = value_n_interaction - value_previous_interaction;
+    return result;
 }
 
 /* imprime a union Float_t como ponto flutuante, hexadecimal e 
