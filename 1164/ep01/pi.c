0e9db850fa7f5b70fbe017100db2bd6dd7aad91e
nar20 <nar20@inf.ufpr.br>
2024-09-18 16:09:01 -0300

    add fsetround(upward) results
    Signed-off-by: nar20 <nar20@inf.ufpr.br>

--- 1164/ep01/pi.c
+++ 1164/ep01/pi.c
@@ -27,29 +27,48 @@ int main(int argc, char const *argv[]){
   value_n_interaction.f = 0;
   //value_n_interaction.f = nextInteraction(interaction_quantity);
   Float_t value_previous_interaction;
-  /* End of variable declaration section */
+  Float_t pi_fedownward;
+/* End of variable declaration section */
+
+
+
   while(!lowerThanTolerance(tolerance, aprox_absolute_error)){
     value_previous_interaction.f = value_n_interaction.f;
     value_n_interaction.f += nextInteraction(interaction_quantity);
     interaction_quantity++;
     aprox_absolute_error.f = calculateNewError(value_n_interaction.f, value_previous_interaction.f);
   }
-    
-  real_absolute_error.f = calculateNewError(M_PI, value_n_interaction.f);
+
+  pi_fedownward.f = value_n_interaction.f;
+  fesetround(FE_UPWARD);
+  value_n_interaction.f = 0;
+  interaction_quantity = 0;
+  aprox_absolute_error.f = tolerance.f+1;
+
+
+  while(!lowerThanTolerance(tolerance, aprox_absolute_error)){
+    value_previous_interaction.f = value_n_interaction.f;
+    value_n_interaction.f += nextInteraction(interaction_quantity);
+    interaction_quantity++;
+    aprox_absolute_error.f = calculateNewError(value_n_interaction.f, value_previous_interaction.f);
+
+  }
+
+  real_absolute_error.f = calculateNewError(value_n_interaction.f, M_PI);
 
   /* Hex convertion section */
   value_n_interaction.i = doubleToHexCoverter(value_n_interaction.f);
-  value_previous_interaction.i = doubleToHexCoverter(value_previous_interaction.f);
+  pi_fedownward.i = doubleToHexCoverter(pi_fedownward.f);
   aprox_absolute_error.i = doubleToHexCoverter(aprox_absolute_error.f);
   real_absolute_error.i = doubleToHexCoverter(real_absolute_error.f);
   /* End of hex convertion section */
-  ulps = value_n_interaction.i - value_previous_interaction.i;
+  ulps = value_n_interaction.i - pi_fedownward.i - 1;;
   /* Printing section */
   printf("%d\n", interaction_quantity);
   printFloat_t(aprox_absolute_error);
   printFloat_t(real_absolute_error);
+  printFloat_t(pi_fedownward);
   printFloat_t(value_n_interaction);
-  printFloat_t(value_previous_interaction);
   printf("%d\n", ulps);
   /* End of printing section */
 
