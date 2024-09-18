46aa0bd3321bda6218d2b8318d0853e665f67d63
nar20 <nar20@inf.ufpr.br>
2024-09-18 16:28:05 -0300

    fix printing specifications
    Signed-off-by: nar20 <nar20@inf.ufpr.br>

--- 1164/ep01/pi.c
+++ 1164/ep01/pi.c
@@ -12,11 +12,11 @@
 
 
 int main(int argc, char const *argv[]){
+
   /* Variable declaration section */
   fesetround(FE_DOWNWARD);
   Float_t tolerance;
   int ulps;
-  printf("Entre com a tolerancia: ");
   scanf("%lf", &tolerance.f);
 
   int interaction_quantity = 0;
@@ -25,20 +25,21 @@ int main(int argc, char const *argv[]){
   aprox_absolute_error.f = tolerance.f+1;
   Float_t value_n_interaction;
   value_n_interaction.f = 0;
-  //value_n_interaction.f = nextInteraction(interaction_quantity);
   Float_t value_previous_interaction;
   Float_t pi_fedownward;
-/* End of variable declaration section */
+  /* End of variable declaration section */
 
 
-
+  /* Start of fe_downward rounding summatory */
   while(!lowerThanTolerance(tolerance, aprox_absolute_error)){
     value_previous_interaction.f = value_n_interaction.f;
     value_n_interaction.f += nextInteraction(interaction_quantity);
     interaction_quantity++;
     aprox_absolute_error.f = calculateNewError(value_n_interaction.f, value_previous_interaction.f);
   }
+  /* End of fe_downward rounding summatory */
 
+  /* Start of fe_upward summatory */
   pi_fedownward.f = value_n_interaction.f;
   fesetround(FE_UPWARD);
   value_n_interaction.f = 0;
@@ -55,6 +56,7 @@ int main(int argc, char const *argv[]){
   }
 
   real_absolute_error.f = calculateNewError(value_n_interaction.f, M_PI);
+  /* End of fe_upward rounding summatory */
 
   /* Hex convertion section */
   value_n_interaction.i = doubleToHexCoverter(value_n_interaction.f);
@@ -62,7 +64,9 @@ int main(int argc, char const *argv[]){
   aprox_absolute_error.i = doubleToHexCoverter(aprox_absolute_error.f);
   real_absolute_error.i = doubleToHexCoverter(real_absolute_error.f);
   /* End of hex convertion section */
+
   ulps = value_n_interaction.i - pi_fedownward.i - 1;;
+
   /* Printing section */
   printf("%d\n", interaction_quantity);
   printFloat_t(aprox_absolute_error);
@@ -72,6 +76,5 @@ int main(int argc, char const *argv[]){
   printf("%d\n", ulps);
   /* End of printing section */
 
-  
   return 0;
 }
