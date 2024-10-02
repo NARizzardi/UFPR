cd49ebf6d5ff99e9ab6d0d7cebc183378344ebb0
Nicolas Rizzardi <rizzanic2000@gmail.com>
2024-10-02 16:55:27 -0300

    [add] exercicio sistema linear
    Signed-off-by: NICOLAS ANDRE RIZZARDI <nar20@inf.ufpr.br>

---
+++ 1164/LIKWID/sistemas_lineares/perSL.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <fenv.h>
+#include "utils.h"
+
+#define MAXIT 50
+
+void imprime_resultado(int tipo, int quantidade_interacoes, double **matriz ){}
+
+void eliminacao_gauss(){}
+
+void gauss_seidel(){}
+
+int main(int argc, char const *argv[]){
+  int ordem;
+  scanf("%d", &ordem);
+  double **matriz_extendida;
+
+  for(int i = 0; i < ordem; i++){
+    for(int j = 0; j <= ordem; j++){
+    }
+  }
+
+
+
+  return 0;
+}
+
---
+++ 1164/LIKWID/teste_likwid/teste.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+METRICA="FLOPS_DP"
+CPU=3
+
+LIKWID_HOME=/home/soft/likwid
+CFLAGS="-I${LIKWID_HOME}/include -DLIKWID_PERFMON"
+LFLAGS="-L${LIKWID_HOME}/lib -llikwid"
+SCALING_FREQ="/sys/devices/system/cpu/cpufreq/policy${CPU}/scaling_governor"
+
+if [ -w ${SCALING_FREQ} ]; then
+    echo "performance" > ${SCALING_FREQ}
+fi
+
+gcc ${CFLAGS} -O0 teste.c -o teste ${LFLAGS}
+for k in $METRICA
+do
+    likwid-perfctr -C ${CPU} -g ${k} -m ./teste >${k}_SemOtimiz.log
+done
+
+rm -f teste
+gcc ${CFLAGS} -O3 teste.c -o teste ${LFLAGS}
+for k in $METRICA
+do
+    likwid-perfctr -C ${CPU} -g ${k} -m ./teste >${k}_Otimiz.log
+done
+
+if [ -w ${SCALING_FREQ} ]; then
+    echo "powersave" > ${SCALING_FREQ}
+fi
+
+echo ''
+echo '-----------------------------------'
+if [ -f FLOPS_DP_Otimiz.log ]; then
+    echo 'Configuração LIKWID: SUCESSO'
+    echo ''
+    ls FLOPS_*.log
+    echo ''
+else
+    echo 'Configuração LIKWID: FALHA'
+fi
+echo '-----------------------------------'
+echo ''
+
